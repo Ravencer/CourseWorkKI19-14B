bb0bdc287ec7a2bb9c3ac7af600ed3a5c4ed65d0
Ravencer <ravencer880@gmail.com>
2021-04-26 13:43:53 +0700

    JS

--- js/main.js
+++ js/main.js
@@ -11,42 +11,4 @@ if (document.body.id === 'index' || document.body.id === 'restaurant') {
 		close.addEventListener('click', toggleModal);
 	}
 }
-if (document.body.id === 'auth-page') {
-	const	signUpBtn = document.getElementById('signUpBtn'),
-		signInBtn = document.getElementById('signInBtn'),
-		signUpInputs = document.querySelectorAll('#signUpInput'),
-		signInInputs = document.querySelectorAll('#signInInput'),
-		resetText = document.getElementById('resetText'),
-		resetInput = document.getElementById('resetInput'),
-		resetBtn = document.getElementById('resetBtn');
-	const toggleInputs = elem => {
-		elem.classList.toggle('input--active');
-	};
-	resetText.addEventListener('click', () => {
-		signInInputs.forEach(elem => {
-			if (elem.classList.contains('input--active')) {
-				toggleInputs(elem);
-			}
-		});
-		resetBtn.style.display = "block";
-		signInBtn.style.display = 'none';
-		resetText.style.display = 'none';
-		signUpBtn.style.display = 'none';
-		toggleInputs(resetInput);
-	});
-	signUpBtn.addEventListener('click', () => {
-		signInBtn.style.display = 'none';
-		resetText.style.display = 'none';
-		signUpInputs.forEach(elem => {
-			if (!elem.classList.contains('input--active')) {
-				toggleInputs(elem);
-			}
-		});
-		signInInputs.forEach(elem => {
-			if (elem.classList.contains('input--active')) {
-				toggleInputs(elem);
-			}
-		});
-	});
-}
 
